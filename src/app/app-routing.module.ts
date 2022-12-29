52e67f0f6e5f96d9aa8215962005228be2662dc2
ClaytonEduard <clayton_eduard@hotmail.com>
2022-12-29 19:47:05 -0300

    :rocket: firt commit - projet config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // se minha rota estiver vazia vai para rota de cursos
+  {
+    path: '', pathMatch: 'full', redirectTo: 'courses'
+  },
+  //rota lazy load
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
