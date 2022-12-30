8a505eec893cf40d0c0d623b3dac5c091aff97c3
ClaytonEduard <clayton_eduard@hotmail.com>
2022-12-30 10:59:46 -0300

    Configurando icons conforme categoria usando pipe

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { CoursesRoutingModule } from 'src/app/courses/courses-routing.module';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   exports: [
@@ -15,7 +16,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatToolbarModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
 })
 export class AppMaterialModule { }
