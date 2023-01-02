126d8fd43b5b80121be2d526dd0dbef33c68ae85
ClaytonEduard <clayton_eduard@hotmail.com>
2023-01-02 11:26:48 -0300

    Passando os dados de edicao para o formulario

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,7 +1,9 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, NonNullableFormBuilder, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../../model/course';
 
 import { CoursesService } from '../../services/courses.service';
 
@@ -12,7 +14,8 @@ import { CoursesService } from '../../services/courses.service';
 })
 export class CourseFormComponent implements OnInit {
 
-  form= this.formBuilder.group({
+  form = this.formBuilder.group({
+    _id: [''],
     name: [''],
     category: [''],
   });
@@ -20,12 +23,21 @@ export class CourseFormComponent implements OnInit {
   constructor(private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private location: Location
+    private location: Location,
+    private route: ActivatedRoute
   ) {
     //this.form
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // pegando os dados para edicao
+    const course: Course = this.route.snapshot.data['course'];
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
+    })
+  }
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(result => this.onSuccess(),
@@ -37,7 +49,7 @@ export class CourseFormComponent implements OnInit {
   }
 
   // mensagem de sucesso
-  private onSuccess(){
+  private onSuccess() {
     this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
     this.onCancel();// voltar a pagina
   }
